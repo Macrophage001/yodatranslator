7021fa6ce5be7562ed48818bee6cfe739fccfa3d
Darnell Champen <zerosalvation@gmail.com>
2022-06-24 10:28:24 -0400

    - added textbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import React, { useState } from 'react'
 import './App.css';
 
+const Textbox = ({ text, setText }) => {
+  const handleChange = (e) => {
+    setText(e.target.value);
+  }
+  const handleSubmit = (e) => {
+    console.log(e.target.value);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <textarea name="Textbox" id="textbox" cols="30" rows="10" value={text} onChange={handleChange}></textarea>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  )
+}
+
 const App = () => {
+  const [text, setText] = useState("");
+  const [translatedText, setTranslatedText] = useState("");
+
   return (
     <div className="App">
+      <Textbox text={text} setText={setText} />
     </div>
   );
 }
